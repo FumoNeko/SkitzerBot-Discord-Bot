6216d5cbac4aa32bc31e4a0a4e8eaa6f41a44baa
FumoNeko <28814987+FumoNeko@users.noreply.github.com>
2021-08-08 02:54:14 -0700

    Setup guild and channel vars, bugfixes

--- bin/bot.py
+++ bin/bot.py
@@ -10,22 +10,20 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 
 client = discord.Client()
 
-roleGreeter = "<@&873675914578378842>"
-rules = "873296117629079613"
-nameChange = "872511776565129316"
-announcement = "872351515661201449"
-roleAssign = "872367240882814976"
-
 @client.event
 async def on_ready():
     print(f'{client.user.name} has connected to Discord!')
 
 @client.event
 async def on_member_join(member):
+    guild = client.get_guild(783515548192866345)
+    channel = guild.get_channel(872356789822312479)
     await channel.send(
-        f'Hi {member.name}, welcome to my Discord server!'
-        f"<@&873675914578378842> \n **Please welcome {member.name} !!!** \nRules are posted in 873296117629079613 \n Name change requests go here: 872511776565129316 \n Announcements are posted in 872351515661201449 \n"
-        .." And lastly! Don't forget to assign your roles in 872367240882814976.. !"
+        f"<@&873675914578378842> \n **Please welcome** {member.mention} **!!!** \n Rules are posted in <#873296117629079613> \n Name change requests go here: <#872511776565129316> \n Announcements are posted in <#872351515661201449> \n"
+        .." And lastly! Don't forget to assign your roles in <#872367240882814976> !"
     )
+
+    if member == client.user:
+        return
 
 client.run(TOKEN)
