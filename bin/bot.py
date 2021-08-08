08fbf2d9a3513324de351c89261e7cafb0dde59b
FumoNeko <28814987+FumoNeko@users.noreply.github.com>
2021-08-08 03:01:54 -0700

    additional comments

--- bin/bot.py
+++ bin/bot.py
@@ -19,8 +19,8 @@ async def on_ready():
 # On member join event
 @client.event
 async def on_member_join(member):
-    guild = client.get_guild(783515548192866345)
-    channel = guild.get_channel(872356789822312479)
+    guild = client.get_guild(783515548192866345) # server id for the server the bot will use
+    channel = guild.get_channel(872356789822312479) # channel the bot will send the message to
     await channel.send(
         f"<@&873675914578378842> \n **Please welcome** {member.mention} **!!!** \n Rules are posted in <#873296117629079613> \n Name change requests go here: <#872511776565129316> \n Announcements are posted in <#872351515661201449> \n"
         .." And lastly! Don't forget to assign your roles in <#872367240882814976> !"
